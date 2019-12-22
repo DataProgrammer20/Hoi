6f57b0787bbac8394d85fafe561d3032d235c755
William <William@BEASTMACHINE>
2019-12-22 16:51:14 -0700

    Creating Contact and Message classes.

---
+++ app/src/main/java/com/william/hoi/data/contact/Contact.kt
@@ -0,0 +1,7 @@
+package com.william.hoi.data.contact
+
+data class Contact(
+    val id: String,
+    val name: String,
+    val email: String
+)
---
+++ app/src/main/java/com/william/hoi/data/message/Message.kt
@@ -0,0 +1,7 @@
+package com.william.hoi.data.message
+
+data class Message(
+    val senderID: String,
+    val text: String,
+    val dateSent: String
+)
